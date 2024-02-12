aa04e87fcdd2cec77892951aa80cf1c59a7677fc
Andres Henriquez <rymbell@gmail.com>
2024-02-12 15:17:01 +0900

    added french option to helloworld tdd

--- go-hello-world/helloworld.go
+++ go-hello-world/helloworld.go
@@ -3,14 +3,22 @@ package main
 import "fmt"
 
 const prefixHello = "Hello, "
+const prefixHola = "Hola, "
+const prefixBonjour = "Bonjour, "
 
-func Hello(name string) string {
+func Hello(name string, language string) string {
 	if name == "" {
 		name = "world"
+	}
+	if language == "Spanish" {
+		return prefixHola + name
+	}
+	if language == "French" {
+		return prefixBonjour + name
 	}
 	return prefixHello + name
 }
 
 func main() {
-	fmt.Println(Hello("Andres"))
+	fmt.Println(Hello("Andres", ""))
 }
