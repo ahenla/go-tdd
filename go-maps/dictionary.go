b01ba40928d80e6b6173c7619cc0bd7f5e02b246
Andres Henriquez <rymbell@gmail.com>
2024-02-21 14:30:48 +0900

    added function for searching in the dictionary and test for cases of found and unfound words

--- go-maps/dictionary.go
+++ go-maps/dictionary.go
@@ -1,5 +1,16 @@
 package main
 
-func Search(dictionary map[string]string, word string) string {
-	return ""
+import "errors"
+
+var ErrNotFound = errors.New("could not find the word you were looking for")
+
+type Dictionary map[string]string
+
+func (d Dictionary) Search(word string) (string, error) {
+	definition, ok := d[word]
+	if !ok {
+		return "", ErrNotFound
+	}
+
+	return definition, nil
 }
