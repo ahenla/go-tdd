c339f576485589dc8165e47bbf983195029bf3f2
Andres Henriquez <rymbell@gmail.com>
2024-03-04 16:48:17 +0900

    refactored the racer function to be more easy to test

--- go-select/racer_test.go
+++ go-select/racer_test.go
@@ -21,7 +21,11 @@ func TestRacer(t *testing.T) {
 			fastURL := fastServer.URL
 
 			want := fastURL
-			got, _ := Racer(slowURL, fastURL)
+			got, err := Racer(slowURL, fastURL)
+
+			if err != nil {
+				t.Fatalf("did not expect an error but i got one")
+			}
 
 			if got != want {
 				t.Errorf("got %q want %q", got, want)
@@ -31,13 +35,11 @@ func TestRacer(t *testing.T) {
 	t.Run(
 		"returns an error if the server doesn't respond within 10 seconds",
 		func(t *testing.T) {
-			serverA := makeDelayedServer(11 * time.Second)
-			serverB := makeDelayedServer(12 * time.Second)
+			server := makeDelayedServer(25 * time.Second)
 
-			defer serverA.Close()
-			defer serverB.Close()
+			defer server.Close()
 
-			_, err := Racer(serverA.URL, serverB.URL)
+			_, err := ConfigurableRacer(server.URL, server.URL, 20*time.Millisecond)
 
 			if err == nil {
 				t.Errorf("expected an error but didn't get one")
