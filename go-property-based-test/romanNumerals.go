a8b952f03013d96960f3359dde08a58c1a922957
Andres Henriquez <rymbell@gmail.com>
2024-03-14 15:46:36 +0900

    the function can convert from 1 to 4 to roman numerals and test to prove it

--- go-property-based-test/romanNumerals.go
+++ go-property-based-test/romanNumerals.go
@@ -1,5 +1,17 @@
 package main
 
+import "strings"
+
 func ConvertToRoman(arabic int) string {
-	return ""
+	if arabic == 4 {
+		return "IV"
+	}
+
+	var result strings.Builder
+
+	for i := 0; i < arabic; i++ {
+		result.WriteString("I")
+	}
+
+	return result.String()
 }
