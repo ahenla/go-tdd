52d7eb16af82ff0d09c3b99f615eb4c6a7c619fb
Andres Henriquez <rymbell@gmail.com>
2024-03-22 15:57:59 +0900

    refactor to cre

--- go-reading-files/blogposts.go
+++ go-reading-files/blogposts.go
@@ -1,13 +1,15 @@
 package blogposts
 
 import (
+	"bufio"
 	"errors"
 	"io"
 	"io/fs"
 )
 
 type Post struct {
-	Title string
+	Title       string
+	Description string
 }
 
 type StubFailingFS struct {
@@ -27,7 +29,7 @@ func NewPostsFromFS(fileSystem fs.FS) ([]Post, error) {
 	var posts []Post
 
 	for _, f := range dir {
-		post, err := getPost(fileSystem, f)
+		post, err := getPost(fileSystem, f.Name())
 		if err != nil {
 			return nil, err // todo: should we totally fail if one file fails? or just ignore?
 		}
@@ -37,21 +39,25 @@ func NewPostsFromFS(fileSystem fs.FS) ([]Post, error) {
 	return posts, nil
 }
 
-func GetPost(fileSystem fs.FS, f fs.DirEntry) (Post, error) {
-	postFile, err := fileSystem.Open(f.Name())
+func getPost(fileSystem fs.FS, fileName string) (Post, error) {
+	postFile, err := fileSystem.Open(fileName)
 
 	if err != nil {
 		return Post{}, err
 	}
 	defer postFile.Close()
 
-	postData, err := io.ReadAll(postFile)
+	return newPost(postFile)
+}
 
-	if err != nil {
-		return Post{}, err
-	}
+func newPost(postFile io.Reader) (Post, error) {
+	scanner := bufio.NewScanner(postFile)
 
-	post := Post{Title: string(postData)[7:]}
-	return post, nil
+	scanner.Scan()
+	titleLine := scanner.Text()
 
+	scanner.Scan()
+	descriptionLine := scanner.Text()
+
+	return Post{Title: titleLine[7:], Description: descriptionLine[13:]}, nil
 }
--- go-reading-files/blogposts_test.go
+++ go-reading-files/blogposts_test.go
@@ -8,9 +8,15 @@ import (
 )
 
 func TestNewBlogPosts(t *testing.T) {
+	const (
+		firstBody = `Title: Post 1
+Description: Description 1`
+		secondBody = `Title: Post 2
+Description: Description 2`
+	)
 	fs := fstest.MapFS{
-		"hello world.md":  {Data: []byte("hi")},
-		"hello-world2.md": {Data: []byte("hola")},
+		"hello world.md":  {Data: []byte(firstBody)},
+		"hello-world2.md": {Data: []byte(secondBody)},
 	}
 
 	posts, err := blogposts.NewPostsFromFS(fs)
@@ -23,10 +29,15 @@ func TestNewBlogPosts(t *testing.T) {
 		t.Errorf("got %d posts, want %d posts", len(posts), len(fs))
 	}
 
-	got := posts[0]
-	want := blogposts.Post{Title: "Post 1"}
+	assertPost(t, posts[0], blogposts.Post{
+		Title:       "Post 1",
+		Description: "Description 1",
+	})
+}
 
+func assertPost(t *testing.T, got blogposts.Post, want blogposts.Post) {
+	t.Helper()
 	if !reflect.DeepEqual(got, want) {
-		t.Errorf("got %+v, want %+v", got, want)
+		t.Errorf("got %v, want %v", got, want)
 	}
 }
